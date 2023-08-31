404473b29e2a775f7702e6980a6248ac6e4ec5cd
Chris Pikul <chris@workmaple.com>
2023-08-31 15:20:52 +0300

    added showInfo utility

--- src/lib/display.ts
+++ src/lib/display.ts
@@ -12,3 +12,13 @@ import { window } from 'vscode';
 export function showError(message: string, ...items: string[]): void {
     window.showErrorMessage(message, ...items);
 }
+
+/**
+ * Shortcut for showing an info message on screen.
+ * 
+ * @param message Message to desplay
+ * @param items Additional message items, these are usually buttons
+ */
+export function showInfo(message: string, ...items: string[]): void {
+    window.showInformationMessage(message, ...items);
+}
